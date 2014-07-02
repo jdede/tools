8050d8e2292dee1a2eb9507bda401879684297a3
Jens Dede <mail@jdede.de>
2014-07-02 14:31:26 +0200

    Merged with contiki version

--- misc/multiRun.py
+++ misc/multiRun.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 
-# multiRun.py
+# Copyright (c) 2014, Jens Dede <jd@comnets.uni-bremen.de>.
+# All rights reserved.
 #
-# Jens Dede, 2014
-# jd@comnets.uni-bremen.de
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# multiRun.py - run an exported cooja simulation several times
 #
-# App to run jar files several times for example for a montecarlo simulation.
-#
-# I wrote this script to execute an exported cooja simulation (--> contiki)
-# several times using multiple processor cores.
-#
+# Cooja can export a complete simulation setup to a jar file. This can be
+# executed independently from the contiki / cooja environment.
+# This python script allows to execute one simulation file
+# several times with different seeds. This may be useful for example for
+# montecarlo evaluations of a simulation setup.
 
 import argparse
 import threading
@@ -130,7 +154,7 @@ class executeThread(threading.Thread):
 ##
 
 parser = argparse.ArgumentParser(
-        description='Run multiple jar files in separate directories and' +\
+        description='Run multiple jar files in separate directories and ' +\
                 'threads using the command "java -jar <progname>"')
 parser.add_argument('progname', help='Name of jar file')
 parser.add_argument('--max_threads', type=int, default=4, dest='max_threads',
